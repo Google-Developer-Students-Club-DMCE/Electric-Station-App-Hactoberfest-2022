f4b6960165c665cbddab3589257c6d8f2fd7cb4e
Akshat Bhuhagal <akshatbhuhagal@gmail.com>
2022-10-26 01:10:52 +0530

    Splash Screen Implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:electric_station/presentation/screens/LoginScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MyApp());
 }
 
@@ -17,7 +22,54 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyLogin(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late Timer timer;
+
+  @override
+  void initState() {
+    timer = Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const MyLogin(),
+            )));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/icons/splash_ic.png",
+            width: MediaQuery.of(context).size.width / 2,
+            height: MediaQuery.of(context).size.height,
+          ),
+        ],
+      ),
     );
   }
 }
